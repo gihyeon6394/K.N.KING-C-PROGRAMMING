75aa0bc97dbce0cab0d8337bcf2fc8d7bc011a57
kgh <gihyeon6394@gmail.com>
2023-06-23 14:08:49 +0900

    feat  : IPC 예제 추가

---
+++ other/process/exampleIPC/mach.c
@@ -0,0 +1,44 @@
+//
+// Created by kkh04511 on 2023-06-23.
+//
+#include<mach/mach.h>
+
+struct message {
+    mach msg header t header;
+    int data;
+};
+
+mach port t client;
+mach port t server;
+
+/* Client Code */
+struct message message;
+
+// construct the header
+message.header.msgh size = sizeof(message);
+message.header.msgh remote port = server;
+message.header.msgh local port = client;
+
+// send the message
+mach msg(&message.header, // message header
+        MACH SEND MSG, // sending a message
+        sizeof(message), // size of message sent
+        0, // maximum size of received message - unnecessary
+        MACH PORT NULL, // name of receive port - unnecessary
+        MACH MSG TIMEOUT NONE, // no time outs
+        MACH PORT NULL // no notify port
+);
+
+
+/* Server Code */
+struct message message;
+
+// receive the message
+mach msg(&message.header, // message header
+        MACH RCV MSG, // sending a message
+        0, // size of message sent
+        sizeof(message), // maximum size of received message
+        server, // name of receive port
+        MACH MSG TIMEOUT NONE, // no time outs
+        MACH PORT NULL // no notify port
+);
